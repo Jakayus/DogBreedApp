a06607dc227645f75edaa5707fe01100466e443c
Joel Sereno <jakayus123@gmail.com>
2023-10-21 11:31:38 -0700

    Initial Project Creation

---
+++ DogBreeds/DogBreeds/DogBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsApp.swift
+//  DogBreeds
+//
+//  Created by Joel Sereno on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
