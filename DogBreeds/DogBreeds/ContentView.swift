5054e3a233fd1dc3cace25247f130c33bef1f797
Joel Sereno <jakayus123@gmail.com>
2023-10-21 16:05:01 -0700

    Adjust HomeScreen to include random dog image

--- DogBreeds/DogBreeds/ContentView.swift
+++ DogBreeds/DogBreeds/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     }
     
     @State private var tabSelection = Tab.Home
+
     
     var body: some View {
         ZStack {
@@ -26,9 +27,9 @@ struct ContentView: View {
                 DogBreedsView()
                     .tabItem { Label("Dog Breeds", systemImage: "dog") }
                     .tag(Tab.DogBreeds)
-//                RandomDogView()
-//                    .tabItem { Label("Random Dog", systemImage: "photo")}
-//                    .tag(Tab.RandomDog)
+                //                RandomDogView()
+                //                    .tabItem { Label("Random Dog", systemImage: "photo")}
+                //                    .tag(Tab.RandomDog)
             }
         }
     }
@@ -40,36 +41,103 @@ struct ContentView: View {
 
 
 struct HomeScreenView: View {
+    
+    @State private var dogImageURLString = ""
+    
     var body: some View {
-            VStack {
-                Image(.header)
-                    .padding(.top)
-                Spacer()
-                Image(.hero)
-                    .resizable()
-                    .scaledToFit()
-                VStack(spacing: 20){
-                    Text("Can you recognize differnet dog breeds?")
-                        .font(.largeTitle)
+        VStack {
+            Image(.header)
+                .padding(.top)
+            Spacer()
+            // using Apple example of error handling for AsyncImage
+            AsyncImage(url: URL(string: dogImageURLString ?? "no image")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(5)
                         .padding()
-                    Text("")
-                        .font(.headline)
-                        .padding()
+                    
+                } else if phase.error != nil {
+                    Label("Could not load image", systemImage: "photo")
+                        .foregroundColor(Color("Secondary1"))
+                } else {
+                    ProgressView("Loading...")
+                        .foregroundColor(Color.white)
+                        .tint(Color.white)
+                    
                 }
-                Spacer()
+            } // end AsyncImage
+            Button("Demo") {
+                grabRandomDogImageURL()
             }
+            Image(.hero)
+                .resizable()
+                .scaledToFit()
+            VStack(spacing: 20){
+                Text("Can you recognize differnet dog breeds?")
+                    .font(.largeTitle)
+                    .padding()
+                Text("")
+                    .font(.headline)
+                    .padding()
+            }
+            Spacer()
+        }
+    } // end View
+        
+        func grabRandomDogImageURL() -> String {
+            
+            var dogImage = ""
+            // get JSON URL
+            let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
+            
+            // create URL request
+            let urlRequest = URLRequest(url: url)
+            
+            // create URL Session task
+            let task = URLSession.shared.dataTask(with: urlRequest) { data, reponse, error in
+                
+                if let data = data, let string = String(data: data, encoding: .utf8) {
+                    
+                    // convert string to Data format
+                    let DogData = Data(string.utf8)
+                    
+                    // create decoder and use it to decode based off of MenuList created earlier
+                    let decoder = JSONDecoder()
+                    let dogImageURLItem = try! decoder.decode(RandomDogImageItem.self, from: DogData)
+                    
+                    //print(dogImageURLString.message)
+                    //dogImage = URL(string: dogImageURLString.message)
+                    dogImageURLString = dogImageURLItem.message
+                    print(dogImage)
+                    
+                } else {
+                    print("unsuccessful decoding - data not valid")
+                }
+            }
+            task.resume()
+            
+            return dogImage
+        } // end function
         
     }
-}
-
-struct DogBreedsView: View {
-    var body: some View {
-        Text("Coming Soon!")
+    
+    struct DogBreedsView: View {
+        var body: some View {
+            Text("Coming Soon!")
+        }
     }
-}
-
-struct RandomDogView: View {
-    var body: some View {
-        Text("Coming Soon!")
+    
+    struct RandomDogView: View {
+        var body: some View {
+            Text("Coming Soon!")
+        }
     }
-}
+    
+    
+    // must decode dog string first
+    struct RandomDogImageItem: Decodable {
+        let message: String
+        let status: String
+    }
