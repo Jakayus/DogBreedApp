79f096fddad90bffbfb3ae9c0b4742998f6d12ed
Joel Sereno <jakayus123@gmail.com>
2023-10-21 17:36:16 -0700

    Add random dog image to HomeScreen
    Add random dog image functionality to HomeScreen. Update UI accordingly.

--- DogBreeds/DogBreeds/ContentView.swift
+++ DogBreeds/DogBreeds/ContentView.swift
@@ -18,19 +18,14 @@ struct ContentView: View {
 
     
     var body: some View {
-        ZStack {
-            Color.blue
-            TabView(selection: $tabSelection){
-                HomeScreenView()
-                    .tabItem { Label("Home", systemImage: "house") }
-                    .tag(Tab.Home)
-                DogBreedsView()
-                    .tabItem { Label("Dog Breeds", systemImage: "dog") }
-                    .tag(Tab.DogBreeds)
-                //                RandomDogView()
-                //                    .tabItem { Label("Random Dog", systemImage: "photo")}
-                //                    .tag(Tab.RandomDog)
-            }
+        
+        TabView(selection: $tabSelection){
+            HomeScreenView()
+                .tabItem { Label("Home", systemImage: "house") }
+                .tag(Tab.Home)
+            DogBreedsView()
+                .tabItem { Label("Dog Breeds", systemImage: "dog") }
+                .tag(Tab.DogBreeds)
         }
     }
 }
@@ -43,52 +38,70 @@ struct ContentView: View {
 struct HomeScreenView: View {
     
     @State private var dogImageURLString = ""
+    let defaultDogURLString = "https://images.dog.ceo/breeds/segugio-italian/n02090722_002.jpg"
+    
+    let heroMessage = "Can you recognize different dog breeds?"
+    let heroSubMessage = "Search for breeds and see pictures for familiarity. Or simply tap the button below for a random dog image!"
+    
     
     var body: some View {
-        VStack {
+        ScrollView {
             Image(.header)
-                .padding(.top)
+                .padding(.top, 30)
             Spacer()
-            // using Apple example of error handling for AsyncImage
-            AsyncImage(url: URL(string: dogImageURLString ?? "no image")) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .cornerRadius(5)
-                        .padding()
-                    
-                } else if phase.error != nil {
-                    Label("Could not load image", systemImage: "photo")
-                        .foregroundColor(Color("Secondary1"))
-                } else {
-                    ProgressView("Loading...")
-                        .foregroundColor(Color.white)
-                        .tint(Color.white)
-                    
-                }
-            } // end AsyncImage
-            Button("Demo") {
-                grabRandomDogImageURL()
-            }
             Image(.hero)
                 .resizable()
-                .scaledToFit()
-            VStack(spacing: 20){
-                Text("Can you recognize differnet dog breeds?")
+                .scaledToFill()
+                .clipShape(.rect(cornerRadius: 10))
+                .padding(.horizontal, 5)
+                .padding(.vertical)
+            VStack{
+                Text(heroMessage)
                     .font(.largeTitle)
-                    .padding()
-                Text("")
+                    .padding(.bottom)
+                Text(heroSubMessage)
                     .font(.headline)
-                    .padding()
+                    .padding(.horizontal)
             }
+            // using Apple example of error handling for AsyncImage
+            VStack {
+                AsyncImage(url: URL(string: dogImageURLString )) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .frame(width: 300, height: 250)
+                            .cornerRadius(5)
+                        
+                    } else if phase.error != nil {
+                        Label("Could not load image", systemImage: "photo")
+                            .foregroundColor(Color(.primary1))
+                    } else {
+                        ProgressView("Loading...")
+                            .foregroundColor(Color.white)
+                            .tint(Color.white)
+                        
+                    }
+                }// end AsyncImage
+                
+                Button {
+                    grabRandomDogImageURL()
+                } label: {
+                    Text("Next Dog 🐶")
+                        .frame(maxWidth: 150)
+                }
+                .buttonStyle(.bordered)
+                .foregroundColor(Color.black)
+                .cornerRadius(5)
+                .padding(.vertical)
+            } // end VStack
+            
+
             Spacer()
         }
     } // end View
         
-        func grabRandomDogImageURL() -> String {
+        func grabRandomDogImageURL() {
             
-            var dogImage = ""
             // get JSON URL
             let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
             
@@ -103,22 +116,19 @@ struct HomeScreenView: View {
                     // convert string to Data format
                     let DogData = Data(string.utf8)
                     
-                    // create decoder and use it to decode based off of MenuList created earlier
+                    // create decoder and use it to decode based off of RandomDogImageItem
                     let decoder = JSONDecoder()
                     let dogImageURLItem = try! decoder.decode(RandomDogImageItem.self, from: DogData)
                     
                     //print(dogImageURLString.message)
                     //dogImage = URL(string: dogImageURLString.message)
                     dogImageURLString = dogImageURLItem.message
-                    print(dogImage)
                     
                 } else {
                     print("unsuccessful decoding - data not valid")
                 }
             }
             task.resume()
-            
-            return dogImage
         } // end function
         
     }
